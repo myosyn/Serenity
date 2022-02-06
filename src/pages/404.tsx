8206763ed23dc0da9c789206e7d6b1a657886457
yujin <84204420+euphorxa@users.noreply.github.com>
2022-02-05 20:33:20 -0800

    a

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import {
+  Container,
+  Icon,
+  Stack,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  useColorModeValue,  
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+
+import { Logo } from '@/components/Logo';
+
+e
