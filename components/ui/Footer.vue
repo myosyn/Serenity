5ce480b233e07756ba37239abfbfc22c01380e17
yujin <84204420+euphorxa@users.noreply.github.com>
2022-03-26 16:21:21 -0700

    Switched to Vite and Vitest

---
+++ components/ui/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    <div class="right">
+      Copyright © 2021-2022 Myosyn LLC. Licensed under the AGPL.
+    </div>
+    <div class="left">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer"
+}
+</script>
